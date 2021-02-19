87b36c45f901542bb0841ea91a8e5deae0f365ca
ab15963qw <36786222+ab15963qw@users.noreply.github.com>
2021-02-20 03:34:55 +0800

    Create jdbc.java

---
+++ jdbc.java
@@ -0,0 +1,25 @@
+import java.sql.*;
+
+public class jdbc {
+    public static void main(String[] args) {
+        String driver = "com.mysql.jdbc.Driver";
+        String url = "jdbc:mysql://localhost/test";
+        String user = "root";
+        String password = "ab15963qw";
+        try {
+            Class.forName("com.mysql.cj.jdbc.Driver");
+            Connection conn = DriverManager.getConnection(url, user, passw
+
+            if (conn != null && !conn.isClosed()) {
+                System.out.println("資料庫連線測試成功！");
+                conn.close();
+            }
+
+        } catch (ClassNotFoundException e) {
+            System.out.println("找不到驅動程式類別");
+            e.printStackTrace();
+        } catch (SQLException e) {
+            e.printStackTrace();
+        }
+    }
+}
