557eda0e55330842f23cf7e03d1ad1bae6ec8b59
ab15963qw <36786222+ab15963qw@users.noreply.github.com>
2021-02-22 23:10:18 +0800

    Update jdbc.java

--- jdbc.java
+++ jdbc.java
@@ -1,28 +1,31 @@
-import java.sql.*;
-
-public class jdbc {
-    public static void main(String[] args) {
-        String driver = "com.mysql.jdbc.Driver";
-        String url = "jdbc:mysql://localhost/test";
-        String user = "";
-        String password = "";
-        try {
-            Class.forName("com.mysql.cj.jdbc.Driver");
-            Connection conn = DriverManager.getConnection(url, user, password)
-
-            if (conn != null && !conn.isClosed()) {
-                System.out.println("資料庫連線測試成功！");
+import java.sql.*; 
+ 
+public class DBConnectionDemo { 
+    public static void main(String[] args) { 
+        String driver = "com.mysql.jdbc.Driver"; 
+        String url = "jdbc:mysql://localhost:3306/GUESTBOOK"; // database
+        String user = ""; //user
+        String password = ""; //password
+        try { 
+            Class.forName(driver); 
+            Connection conn = 
+               DriverManager.getConnection(url, 
+                                  user, password);
+ 
+            if(conn != null && !conn.isClosed()) {
+                System.out.println("資料庫連線測試成功！"); 
                 conn.close();
             }
-
-        } catch (ClassNotFoundException e) {
-            System.out.println("找不到驅動程式類別");
-            e.printStackTrace();
-        } catch (SQLException e) {
-            e.printStackTrace();
-        }
-    }
-}
-
+            
+        } 
+        catch(ClassNotFoundException e) { 
+            System.out.println("找不到驅動程式類別"); 
+            e.printStackTrace(); 
+        } 
+        catch(SQLException e) { 
+            e.printStackTrace(); 
+        } 
+    } 
+} 
 
 
